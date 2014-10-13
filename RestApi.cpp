0d0250587e3a3890b6493d7010927ecef32216f1
Mohammad S. Babaei <info@babaei.net>
2014-10-13 23:20:51 +0330

    Code clean-up inside RestApi class

--- RestApi.cpp
+++ RestApi.cpp
@@ -120,11 +120,8 @@ void RestApi::signIn(const QString &user, const QString &pwd)
 {
     std::wstringstream stream;
     boost::property_tree::wptree tree;
-    //std::string outPwd;
 
     tree.put(L"username", user.toStdWString());
-    //Pool::Crypto()->GenerateHash(pwd.toStdString(), outPwd);
-    //tree.put(L"password", QString::fromStdString(outPwd).toStdWString());
     tree.put(L"password", pwd.toStdWString());
 
     boost::property_tree::write_json(stream, tree);
@@ -139,11 +136,8 @@ void RestApi::signOut(const QString &token, const QString &user, const QString &
 {
     std::wstringstream stream;
     boost::property_tree::wptree tree;
-    //std::string outPwd;
 
     tree.put(L"username", user.toStdWString());
-    //Pool::Crypto()->GenerateHash(pwd.toStdString(), outPwd);
-    //tree.put(L"password", QString::fromStdString(outPwd).toStdWString());
     tree.put(L"password", pwd.toStdWString());
 
     boost::property_tree::write_json(stream, tree);
