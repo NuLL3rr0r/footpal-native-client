1dfe700896c53ee8d8f0db0eacfc6a0a4969736a
M.S. Babaei <info@babaei.net>
2014-11-02 15:14:52 +0330

    Commented Majid's logs

--- deployment/resources/ui/scripts/ws.js
+++ deployment/resources/ui/scripts/ws.js
@@ -16,52 +16,52 @@ function registerSocket(socketIns){
 }
 
 function registerTimer(ti){
-    console.log("socket registered");
+    //console.log("socket registered");
     timer = ti;
 }
 
 function openSocket(){
-//    console.log("try to open socket");
+    //console.log("try to open socket");
     socketJs.active = true;
 }
 
 function parseTextMessage(message){
-    console.log("new message : " + message);
+    //console.log("new message : " + message);
     var encoded = JSON.parse(message);
-    console.log(encoded + " : " + encoded.code);
+    //console.log(encoded + " : " + encoded.code);
     if(encoded.code === 0){
         authorizeToWs(Context.token);
     }
     else if(encoded.code === 1){
-        console.log("Send GetIndividualRooms cmd");
+        //console.log("Send GetIndividualRooms cmd");
         getIndividualRooms();
         getMyProfile();
         getFriendList();
     }
     else if(encoded.code === 5){
-        console.log("IndividualContacts result");
+        //console.log("IndividualContacts result");
         for(var i = 0 ; i < encoded.rooms.length ; i++){
-            console.log(encoded.rooms[i]._id);
+            //console.log(encoded.rooms[i]._id);
             Context.addNewRoom("room", encoded.rooms[i]._id, "", "", "I");
             for(var j = 0 ; j < encoded.rooms[i].Members.length; j++){
-                console.log(encoded.rooms[i].Members[j]);
+                //console.log(encoded.rooms[i].Members[j]);
                 Context.addMemberToRoom(encoded.rooms[i]._id, encoded.rooms[i].Members[j], encoded.rooms[i].Members[j]);
             }
             Context.getRoomById(encoded.rooms[i]._id).log();
         }
     }
     else if(encoded.code === 6){
-        console.log("currentProfile");
+        //console.log("currentProfile");
         Context.currentProfile = encoded.profile;
-        console.log(Context.currentProfile.id + " : " + Context.currentProfile.username);
+        //console.log(Context.currentProfile.id + " : " + Context.currentProfile.username);
     }
     else if(encoded.code === 3){
-        console.log(encoded.message);
-        console.log(encoded.value.roomId);
+        //console.log(encoded.message);
+        //console.log(encoded.value.roomId);
         var room = Context.getRoomById(encoded.value.roomId);
         var self = 'self';
         if(encoded.value.from === Context.currentProfile.username){
-            console.log('self message');
+            //console.log('self message');
         }
         else{
             self = encoded.value.from;
@@ -72,12 +72,12 @@ function parseTextMessage(message){
         }
     }
     else if(encoded.code === 8){
-        console.log("user added to database successfully : " + encoded.friend.friendId);
+        //console.log("user added to database successfully : " + encoded.friend.friendId);
         Context.addFriend(encoded.friend.friendId, encoded.friend.friendUsername, encoded.friend.state   );
     }
     else if(encoded.code === 9 ){
         for(var i = 0 ; i <encoded.friends.length; i++){
-            console.log(encoded.friends[i].friendId + " : " + encoded.friends[i].friendUsername);
+            //console.log(encoded.friends[i].friendId + " : " + encoded.friends[i].friendUsername);
             Context.addFriend(encoded.friends[i].friendId, encoded.friends[i].friendUsername, encoded.friends[i].state   );
         }
     }
@@ -85,12 +85,12 @@ function parseTextMessage(message){
 
 function getMyProfile(){
     var cmd = "{\"requestCode\" : \"4\", \"message\": \"GetCurrentProfile\"}";
-    console.log(cmd)
+    //console.log(cmd)
     socketJs.sendTextMessage(cmd);
 }
 
 function TryToConnectToWS(){
-//    console.log(socketJs.status);
+    //console.log(socketJs.status);
     if(socketJs.status != 0 || socketJs.status != 1)
     {
         socketJs.active = false;
@@ -100,47 +100,47 @@ function TryToConnectToWS(){
 
 function getUserByUserId(userId){
     var cmd = "{\"requestCode\" : \"5\", \"message\": \"GetUsernameViaUserId\"}";
-    console.log(cmd);
+    //console.log(cmd);
     socketJs.sendTextMessage(cmd);
 }
 
 function websocketOpened(){
-    console.log("Socket has been opened");
+    //console.log("Socket has been opened");
     timer.stop();
 }
 
 function webSocketClosed(){
-//    console.log("Socket has been closed");
-//    console.log("try to open about 5 seconds later");
+    //console.log("Socket has been closed");
+    //console.log("try to open about 5 seconds later");
     timer.start();
 }
 
 function webSocketError(err){
-//    console.log("Error: " + err);
+    //console.log("Error: " + err);
     timer.start();
 }
 
 function authorizeToWs(token){
     var cmd = "{\"token\":\"" + token + "\"}";
-    console.log(cmd)
+    //console.log(cmd)
     socketJs.sendTextMessage(cmd);
 }
 
 function getIndividualRooms(){
     var cmd = "{\"requestCode\" : \"3\", \"message\": \"GetIndividualRooms\"}";
-    console.log(cmd);
+    //console.log(cmd);
     socketJs.sendTextMessage(cmd);
 }
 
 function addFriendToList(username){
     var cmd = "{\"requestCode\" : \"6\", \"message\": \"AddUserToFriend\", \"username\" : \"" + username + "\"}";
-    console.log(cmd);
+    //console.log(cmd);
     socketJs.sendTextMessage(cmd);
 }
 
 function getFriendList(){
     var cmd = "{\"requestCode\" : \"7\", \"message\": \"GetFriendList\"}";
-    console.log(cmd);
+    //console.log(cmd);
     socketJs.sendTextMessage(cmd);
 }
 
@@ -156,6 +156,6 @@ function sendTextMessageToRoom(message, to){
 //        }
     }
     catch(ex){
-        console.log(ex);
+        //console.log(ex);
     }
 }
